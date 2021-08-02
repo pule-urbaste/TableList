c3aae26439e7691150860fe236557743f69eb1cd
pule-urbaste <inta.pule@gmail.com>
2021-08-02 18:56:45 +0300

    Initial Commit

---
+++ TableList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableList
+//
+//  Created by inta.pule.urbaste on 02/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
